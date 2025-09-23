1247f3c0269f4264abe5657d9159897d825890a7
Marlon Quinde <mquinde@viamatica.com>
2025-09-23 18:35:30 -0500

    Swagger

--- ERP/Program.cs
+++ ERP/Program.cs
@@ -57,11 +57,8 @@ var app = builder.Build();
 app.UseCors("AllowDev");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
