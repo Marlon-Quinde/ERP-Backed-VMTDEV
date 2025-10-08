67e0b65792db3057e876af5759f4d2b314dc8b91
Marlon Quinde <mquinde@viamatica.com>
2025-10-08 01:59:11 -0500

    test

--- ERP/CoreDB/BaseErpContext.cs
+++ ERP/CoreDB/BaseErpContext.cs
@@ -71,9 +71,9 @@ public partial class BaseErpContext : DbContext
 
     public virtual DbSet<UsuarioRol> UsuarioRols { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=BASE_ERP;Trusted_Connection=True;TrustServerCertificate=Yes");
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseSqlServer("Server=localhost;Database=BASE_ERP;Trusted_Connection=True;TrustServerCertificate=Yes");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
